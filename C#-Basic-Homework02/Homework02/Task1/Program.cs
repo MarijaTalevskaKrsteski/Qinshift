f26b78253e17ce85a2e605d57ff1d7dde7271d23
Marija <talevska.marija@gmail.com>
2024-03-07 17:24:19 +0100

    C#-Basic-Homework02

---
+++ C#-Basic-Homework02/Homework02/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Make a console application called SumOfEven. Inside it create an array of 6 integers. Find and print the sum of the even numbers from the array:
+//Test Data:
+//Enter integer no.1:
+//4
+//Enter integer no.1:
+//3
+//Enter integer no.1:
+//7
+//Enter integer no.1:
+//3
+//Enter integer no.1:
+//2
+//Enter integer no.1:
+//8
+//Expected Output:
+//The result is: 14
+
+Console.WriteLine("SumOfEven\n");
+
+int[] arrayOfIntegers = new int[6];
+int sum = 0;
+bool printSum = false;
+
+for (int i = 0; i < arrayOfIntegers.Length; i++)
+{
+    Console.WriteLine("Enter integer no." + (i+1));
+    if (int.TryParse(Console.ReadLine(), out arrayOfIntegers[i]))
+    {
+        if (arrayOfIntegers[i] % 2 == 0)
+        {
+            sum += arrayOfIntegers[i];
+        }         
+    }
+    else
+    { 
+        Console.WriteLine("Invalid input. Please enter a number.");
+        printSum = true;
+        break;
+    }
+}
+if (!printSum)
+    Console.WriteLine("The result is:" + sum);
---
+++ C#-Basic-Homework02/Homework02/Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿//Make a new console application called StudentGroup
+//Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names (strings).
+//Get a number from the console between 1 and 2 and print the students from that group in the console.
+//Ex: studentsG1["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+//Test Data:
+//Enter student group: (there are 1 and 2 )
+//1
+//Expected Output:
+//The Students in G1 are:
+//Zdravko
+//Petko
+//Stanko
+//Branko
+//Trajko
+
+Console.WriteLine("Student Group\n");
+
+string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = new string[] { "Marija", "Ana", "Ema", "Maja", "Viki" };
+
+Console.WriteLine("Enter student group: (there are 1 and 2):");
+if(int.TryParse(Console.ReadLine(), out int groupNumberInput))
+{
+    if(groupNumberInput == 1)
+    {
+        Console.WriteLine("\nThe students in G1 are:");
+        for (int i = 0; i < studentsG1.Length; i++)
+        {
+            Console.WriteLine(studentsG1[i]);
+        }
+    } 
+    else if (groupNumberInput == 2)
+    {
+        Console.WriteLine("\nThe students in G2 are:");
+        for (int i = 0; i < studentsG2.Length; i++)
+        {
+            Console.WriteLine(studentsG2[i]);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please choose 1 or 2!");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input!");
+}
